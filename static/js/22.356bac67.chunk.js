6ed7b2c9a37a917f34d706d6df2c6c19cc919d4e
W3stside <W3stside@users.noreply.github.com>
2022-07-08 13:19:07 +0000

    deploy: 3293e364965511f678b7b4413ff77776f78bba48

---
+++ static/js/22.356bac67.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonppastelle=this.webpackJsonppastelle||[]).push([[22],{476:function(n,p){},477:function(n,p){},613:function(n,p){}}]);
+//# sourceMappingURL=22.356bac67.chunk.js.map
--- static/js/22.9e81e8d7.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonppastelle=this.webpackJsonppastelle||[]).push([[22],{477:function(n,p){},478:function(n,p){},613:function(n,p){}}]);
-//# sourceMappingURL=22.9e81e8d7.chunk.js.map
